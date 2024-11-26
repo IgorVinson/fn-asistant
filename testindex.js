486d674c7488a39e3cdb801e527a500f4c465417
Igor T <igorvinson@gmail.com>
2024-11-26 12:19:12 -0500

    add express and nodemon

--- testindex.js
+++ testindex.js
@@ -2,8 +2,10 @@ import {loginToFieldNation} from "./utils/loginToFieldNation.js";
 
 const page = await loginToFieldNation('https://app.fieldnation.com/')
 const notifyButtons = await page.$$('.IconButton__3SE5w');
-const notifyButton = notifyButtons[5];
-await notifyButton.click();
+
+console.log(notifyButtons)
+// const notifyButton = notifyButtons[5];
+// await notifyButton.click();
 
 // const allNotifications = await page.waitForXPath.("//span[text()='View All Notifications']");
 // await allNotifications.click();
