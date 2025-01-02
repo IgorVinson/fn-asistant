40c6a6ecad78d877fb3812ac807f508f76895d27
Igor T <igorvinson@gmail.com>
2025-01-01 23:27:43 -0500

    post workOrder

---
+++ utils/sendWorkOrderMessage.js
@@ -0,0 +1,67 @@
+import fs from 'fs';
+import path from 'path';
+
+// Шлях до файлу з куками
+const cookiesFilePath = path.resolve('utils', 'FieldNation', 'cookies.json');
+
+// Функція для отримання куків
+function getCookies() {
+    if (!fs.existsSync(cookiesFilePath)) {
+        throw new Error('Файл куків не знайдено!');
+    }
+    const cookiesJson = JSON.parse(fs.readFileSync(cookiesFilePath, 'utf-8'));
+    return cookiesJson.map(cookie => `${cookie.name}=${cookie.value}`).join('; ');
+}
+
+// Функція для виконання запиту і аналізу даних
+export async function sendWorkOrderMessage(url) {
+    try {
+        // Отримуємо куки
+        const cookies = getCookies();
+        const workOrderId = url.split('?')[0].split('/').pop();
+
+        const message = 'Hello,\n' +
+            'I’m ready to assist with your project and have extensive experience in this field. Please find a link to my portfolio for your review. \n' +
+            'https://drive.google.com/drive/folders/1p_7aGla27KgB4GVIPRjf6wb7t7RN6AE7?usp=sharing\n' +
+            'If selected, I will ensure the highest quality work and your satisfaction.\n' +
+            ' Thank you for your time and consideration!!';
+
+        // Виконуємо запит
+        await fetch(`https://app.fieldnation.com/v2/workorders/${workOrderId}/messages`, {
+            headers: {
+                "accept": "application/json",
+                "accept-language": "en-US,en;q=0.9,uk-UA;q=0.8,uk;q=0.7,ru-UA;q=0.6,ru;q=0.5",
+                "content-type": "application/json",
+                "priority": "u=1, i",
+                "sec-ch-ua": "\"Google Chrome\";v=\"131\", \"Chromium\";v=\"131\", \"Not_A Brand\";v=\"24\"",
+                "sec-ch-ua-mobile": "?0",
+                "sec-ch-ua-platform": "\"macOS\"",
+                "sec-fetch-dest": "empty",
+                "sec-fetch-mode": "cors",
+                "sec-fetch-site": "same-origin",
+                "cookie": cookies,
+                "Referer": "https://app.fieldnation.com/workorders/16391887?work_order_rank=1&work_order_total=34&work_order_list=workorders_available",
+                "Referrer-Policy": "strict-origin-when-cross-origin"
+            },
+            body: JSON.stringify({to: {id: 0}, message , type: "REQUESTEDTECHS", notify: []}
+            ),
+            method: "POST",
+        }).then(
+            response => {
+                if (response.ok) {
+                    console.log("Message sent successfully!");
+                }
+            }
+        ).catch(
+            error => {
+                console.error("Error sending work order request:", error);
+            }
+        )
+
+        // Перевірка статусу відповіді
+    } catch (error) {
+        console.error('Помилка:', error.message);
+        return null;
+    }
+}
+
