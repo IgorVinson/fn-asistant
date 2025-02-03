a2bb63650c28e3299104cb81c41e69b6bd316825
Igor T <igorvinson@gmail.com>
2025-02-03 14:01:56 -0500

    Refactor schedule, eligibility check, and logging
    - Update schedule data structure with new month's events
    - Modify isEligibleForApplication to use logger
    - Update import/export for schedule module
    - Remove unused calendar generation utility
    - Update .gitignore to include additional cookie and log files

--- schedule.js
+++ schedule.js
@@ -1,63 +1,62 @@
 const currentMonth = {
-    "week1": {
-        "WED 1": [],
-        "THU 2": [],
-        "FRI 3": [],
-        "SAT 4": [],
-        "SUN 5": []
-    },
-    "week2": {
-        "MON 6": [],
-        "TUE 7": [],
-        "WED 8": [],
-        "THU 9": [],
-        "FRI 10": [],
-        "SAT 11": [{
-            id: '5659140276',
-            platform: 'FieldNation',
-            title: 'cabling',
-            time: {
-                start: '2025-01-11T16:00:00',
-                end: '2025-01-11T18:00:00'
-            },
-        }
-        ],
-        "SUN 12": []
-    },
-    "week3": {
-        "MON 13": [],
-        "TUE 14": [],
-        "WED 15": [],
-        "THU 16": [],
-        "FRI 17": [],
-        "SAT 18": [],
-        "SUN 19": []
-    },
-    "week4": {
-        "MON 20": [],
-        "TUE 21": [],
-        "WED 22": [],
-        "THU 23": [],
-        "FRI 24": [],
-        "SAT 25": [],
-        "SUN 26": []
-    },
-    "week5": {
-        "MON 27": [{
-            id: '5659140276',
-            platform: 'FieldNation',
-            title: 'cabling',
-            time: {
-                start: '2025-01-27T10:00:00',
-                end: '2025-01-27T12:00:00'
-            },
-        }],
-        "TUE 28": [],
-        "WED 29": [],
-        "THU 30": [],
-        "FRI 31": []
-    }
-}
+  week1: {
+    'SAT 1': [],
+    'SUN 2': [],
+  },
+  week2: {
+    'MON 3': [],
+    'TUE 4': [
+      {
+        id: '16573917',
+        platform: 'FieldNation',
+        title: 'Doorbell installation',
+        time: {
+          start: '2025-01-04T09:00:00',
+          end: '2025-01-04T12:00:00',
+        },
+      },
+    ],
+    'WED 5': [],
+    'THU 6': [],
+    'FRI 7': [],
+    'SAT 8': [],
+    'SUN 9': [],
+  },
+  week3: {
+    'MON 10': [
+      {
+        id: '16556905',
+        platform: 'FieldNation',
+        title: 'Router refresh',
+        time: {
+          start: '2025-02-10T10:30:00',
+          end: '2025-02-10T12:30:00',
+        },
+      },
+    ],
+    'TUE 11': [],
+    'WED 12': [],
+    'THU 13': [],
+    'FRI 14': [],
+    'SAT 15': [],
+    'SUN 16': [],
+  },
+  week4: {
+    'MON 17': [],
+    'TUE 18': [],
+    'WED 19': [],
+    'THU 20': [],
+    'FRI 21': [],
+    'SAT 22': [],
+    'SUN 23': [],
+  },
+  week5: {
+    'MON 24': [],
+    'TUE 25': [],
+    'WED 26': [],
+    'THU 27': [],
+    'FRI 28': [],
+  },
+};
 
-
-export default currentMonth;
+export { currentMonth };
