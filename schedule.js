29963c864a27286fda8b09cdafa6432d9b2dd44f
IgorVinson <igorvinson@gmail.com>
2025-04-17 12:40:43 -0400

    cleanup and update application logic

--- schedule.js
+++ schedule.js
@@ -1,82 +1,45 @@
 const currentMonth = {
-  week1: {
-    'SAT 1': [],
-    'SUN 2': [],
+  "week1": {
+    "TUE 1": [],
+    "WED 2": [],
+    "THU 3": [],
+    "FRI 4": [],
+    "SAT 5": [],
+    "SUN 6": []
   },
-  week2: {
-    'MON 3': [],
-    'TUE 4': [
-      {
-        id: '16573917',
-        platform: 'FieldNation',
-        title: 'Doorbell installation',
-        time: {
-          start: '2025-01-04T09:00:00',
-          end: '2025-01-04T12:00:00',
-        },
-      },
-    ],
-    'WED 5': [],
-    'THU 6': [],
-    'FRI 7': [],
-    'SAT 8': [],
-    'SUN 9': [],
+  "week2": {
+    "MON 7": [],
+    "TUE 8": [],
+    "WED 9": [],
+    "THU 10": [],
+    "FRI 11": [],
+    "SAT 12": [],
+    "SUN 13": []
   },
-  week3: {
-    'MON 10': [
-      {
-        id: '16556905',
-        platform: 'FieldNation',
-        title: 'Router refresh',
-        time: {
-          start: '2025-02-10T10:30:00',
-          end: '2025-02-10T12:30:00',
-        },
-      },
-    ],
-    'TUE 11': [],
-    'WED 12': [],
-    'THU 13': [],
-    'FRI 14': [],
-    'SAT 15': [],
-    'SUN 16': [],
+  "week3": {
+    "MON 14": [],
+    "TUE 15": [],
+    "WED 16": [],
+    "THU 17": [],
+    "FRI 18": [],
+    "SAT 19": [],
+    "SUN 20": []
   },
-  week4: {
-    'MON 17': [
-      {
-        id: '',
-        platform: 'FieldNation',
-        title: 'Printer data run ',
-        time: {
-          start: '2025-02-17T09:00:00',
-          end: '2025-02-10T17:00:00',
-        },
-      },
-    ],
-    'TUE 18': [
-      {
-        id: '',
-        platform: 'FieldNation',
-        title: 'REOPLACEMENT DATA REMOTE INSTALL/ MUST HAVE BUTT-SET-DIS117971 ',
-        time: {
-          start: '2025-02-18T09:00:00',
-          end: '2025-02-18T11:00:00',
-        },
-      },
-    ],
-    'WED 19': [],
-    'THU 20': [],
-    'FRI 21': [],
-    'SAT 22': [],
-    'SUN 23': [],
+  "week4": {
+    "MON 21": [],
+    "TUE 22": [],
+    "WED 23": [],
+    "THU 24": [],
+    "FRI 25": [],
+    "SAT 26": [],
+    "SUN 27": []
   },
-  week5: {
-    'MON 24': [],
-    'TUE 25': [],
-    'WED 26': [],
-    'THU 27': [],
-    'FRI 28': [],
-  },
-};
+  "week5": {
+    "MON 28": [],
+    "TUE 29": [],
+    "WED 30": []
+  }
+}
 
-export { currentMonth };
+
+export default currentMonth;
